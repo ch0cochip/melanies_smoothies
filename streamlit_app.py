234679628123c6af96fc01b5adb8f3b6f358ba3d
Careen Hakim <93469802+ch0cochip@users.noreply.github.com>
2025-10-28 16:40:26 +1100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,8 +22,8 @@ st.write('The name on your Smoothie will be:', name_on_order)
 
 # Convert the Snowpark Dataframe to a Pandas Dataframe so we can use the LOC function
 pd_df=my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+# st.dataframe(pd_df)
+# st.stop()
 
 
 ingredients_list = st.multiselect(
